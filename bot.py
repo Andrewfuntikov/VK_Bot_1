1bd219e20febbda723234c21f7c5c661d7da531b
Андрей <andhunter77@gmail.com>
2024-06-12 14:43:41 +0500

    Добавил новый файл и настроил ответ на баз. вопросы

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+import handlers
 from settings import TOKEN, GROUP_ID
 import vk_api
 import random
@@ -25,11 +26,24 @@ def configure_logging():
     log.setLevel(logging.DEBUG)
 
 
+class UserState:
+    """Состояние пользователя внутри сценария."""
+
+    def __init__(self, scenario_name, step_name, context=None):
+        self.scenario_name = scenario_name
+        self.step_name = step_name
+        self.context = context or {}
+
+
 class Bot:
     """
-    Echo bot для vk.com.
-
+    Сценарий регистрации на конференцию "Skillbox Conf" через vk.com
     Use: python 3.12.3
+    Поддерживает ответы на вопросы про дату, место проведения и сценарий регистрации:
+    - спрашиваем имя
+    - спрашиваем mail
+    - говорим об успешной регистрации
+    Если шаг не пройден, задаем уточняющий вопрос пока шаг не будет пройден.
     """
 
     def __init__(self, token, group_id):
@@ -42,6 +56,7 @@ class Bot:
         self.vk = vk_api.VkApi(token=TOKEN)
         self.long_poller = VkBotLongPoll(self.vk, self.group_id)
         self.api = self.vk.get_api()
+        self.user_states = dict()  # user_id -> UserState
 
     def run(self):
         """Запуск бота."""
@@ -57,15 +72,58 @@ class Bot:
         :param event: VkBotMessageEvent object
         :return: None
         """
-        if event.type == VkBotEventType.MESSAGE_NEW:
-            log.debug("Отправляем сообщение назад")
-            self.api.messages.send(
-                message=event.message.text,
-                random_id=random.randint(0, 2 ** 20),
-                peer_id=event.message.peer_id
-            )
+        if event.type != VkBotEventType.MESSAGE_NEW:
+            log.info("мы пока не умеем обрабатывать событие такого типа %s", event.type)
+            return
+        user_id = event.message.peer_id
+        text = event.message.text
+        if user_id in self.user_states:
+            text_to_send = self.continue_scenario(user_id, text)
         else:
-            log.info(f'Мы пока не умеем обрабатывать события такого типа {event.type}')
+            # search intent
+            for intent in settings.INTENTS:
+                if any(token in text for token in intent['tokens']):
+                    if intent['answer']:
+                        text_to_send = intent['answer']
+                    else:
+                        text_to_send = self.start_scenario(user_id, intent['scenario'])
+                    break
+            else:
+                text_to_send = settings.DEFAULT_ANSWER
+        self.api.messages.send(
+            message=text_to_send,
+            random_id=random.randint(0, 2 ** 20),
+            peer_id=user_id
+        )
+
+    def start_scenario(self, user_id, scenario_name):
+        scenario = settings.SCENARIOS[scenario_name]
+        first_step = scenario['first_step']
+        step = scenario['steps'][first_step]
+        text_to_send = step['text']
+        self.user_states[user_id] = UserState(scenario_name=scenario_name, step_name=first_step)
+        return text_to_send
+
+    def continue_scenario(self, user_id, text):
+        state = self.user_states[user_id]
+        steps = settings.SCENARIOS[state.scenario_name]['steps']
+        step = steps[state.step_name]
+        handler = getattr(handlers, step['handler'])
+        if handler(text=text, context=state.context):
+            # next step
+            next_step = steps[step['next_step']]
+            text_to_send = next_step['text'].format(**state.context)
+            if next_step['next_step']:
+                # switch to next step
+                state.step_name = step['next_step']
+            else:
+                # finish scenario
+                self.user_states.pop(user_id)
+        else:
+            # retry current step
+            text_to_send = step['failure_text'].format(**state.context)
+
+        return text_to_send
 
 
 if __name__ == "__main__":
---
+++ handlers.py
@@ -0,0 +1,26 @@
+"""
+Handler - функция, которая принимает нна вход text (текст входящего сообщения) и context (dict), а возвращает bool:
+True если шаг пройден, False если данные введены не правильно.
+"""
+import re
+
+re_name = re.compile(r'^[\w\-\s]{3, 30}$')
+re_email = re.compile(r"\b[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+\b")
+
+
+def handle_name(text, context):
+    match = re.match(re_name, text)
+    if match:
+        context['name'] = text
+        return True
+    else:
+        return False
+
+
+def handle_email(text, context):
+    matches = re.findall(re_email, text)
+    if len(matches) > 0:
+        context['email'] = matches[0]
+        return True
+    else:
+        return False
